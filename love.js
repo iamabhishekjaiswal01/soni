014bbe65c1e367a1e7cfa8e645318f4c74dac4bd
iamabhishekjaiswal01 <76740404+iamabhishekjaiswal01@users.noreply.github.com>
2020-12-30 03:31:45 +0530

    Update love.js

--- love.js
+++ love.js
@@ -4,10 +4,10 @@ function songcontrol()
 {
 var songElement=document.getElementById("song");
 if (document.body.scrollTop > 750 || document.documentElement.scrollTop > 750) {
-songElement.pause();
+document.getElementById("song").pause();
 }
 else
 {
-songElement.play();
+document.getElementById("song").play();
 }
 }
